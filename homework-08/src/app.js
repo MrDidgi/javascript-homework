970ca8e7a6d2afc0dd6371ad2e2eebdd902f42a5
MrDidgi <hitigor@yandex.ru>
2022-06-22 19:34:49 +0300

    homework 8

--- homework-08/src/app.js
+++ homework-08/src/app.js
@@ -1,3 +1,5 @@
+
+
 const galleryItems = [
   {
     preview:
@@ -63,3 +65,86 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+
+// Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+// Открытие модального окна по клику на элементе галереи.
+// Подмена значения атрибута src элемента img.lightbox__image.
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+// Очистка значения атрибута src элемента img.lightbox__image, 
+// это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+
+
+
+//   ------------- создание разметки для картинок и их внедрение -------------
+const galleryMapping = function (arr) {
+  return arr.map(({preview, description, original}) => 
+  //<a href='${original}' class='gallery__link'></a>
+    `<li class='gallery__item'>
+      <a href='#' class='gallery__link'>
+        <img class='gallery__image' src="${preview}" alt="${description}" data-previewUrl="${original}">
+      </a>
+    </li>`
+  ).join('');
+};
+
+const markupToAppend = galleryMapping(galleryItems);
+
+const ulElement = document.querySelector('.gallery.js-gallery');
+
+ulElement.innerHTML = markupToAppend;
+
+const divLightbox = document.querySelector('div.lightbox');
+
+const imgModal = document.querySelector('img.lightbox__image');
+
+//   ------------- подстановка нужной картинки в модальное окно -------------
+// const modalMapping = function (arr) {
+//   return arr.map(({original, description}) => 
+//    { 
+//     imgModal.setAttribute("src", original),
+//     imgModal.setAttribute("alt", description) 
+//     }
+//   );
+// };
+console.log(imgModal);
+
+//   ------------- модальное окно (превью картинки) -------------
+const imagePreview = function (event) {
+  
+    if (!event.target.classList.contains('gallery__image')) {
+      return; 
+    }
+    
+    if (divLightbox.classList.contains('is-open')) {
+      return
+    }
+
+   //  ------------- подстановка картинки в модалку -------------
+    imgModal.setAttribute("src", event.target.dataset.previewurl);
+    imgModal.setAttribute("alt", event.target.description)
+
+    divLightbox.classList.add('is-open');
+};
+
+ulElement.addEventListener('click', imagePreview);
+
+//  ------------- закрытие модального окна по кнопке -------------
+const closeButton = document.querySelector('button.lightbox__button');
+
+const closeModal = function (event) {
+  if (!event.target.classList.contains('lightbox__button')) {
+    return; 
+  } 
+  divLightbox.classList.remove('is-open');
+  imgModal.setAttribute("src", '');
+  imgModal.setAttribute("alt", ''); 
+ 
+};
+
+closeButton.addEventListener('click', closeModal);
+
+
+
+
